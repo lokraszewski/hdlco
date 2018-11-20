a4c9b6a0ef7db033eed023584a56fb09c133e69c
lokraszewski <lokraszewski@innovative-technology.com>
2018-11-20 14:40:29 +0000

    Auto stash before revert of "Merge branch 'feature/options' into develop"

--- hdlc/include/hdlc/frame_reciever.h
+++ hdlc/include/hdlc/frame_reciever.h
@@ -9,6 +9,7 @@
 
 #include "types.h"
 #include <boost/circular_buffer.hpp>
+#include <vector>
 
 namespace hdlc
 {
@@ -69,32 +70,99 @@ public:
 
     return success;
   }
+    void recieve(const uint8_t byte)
+    {
+      if (full() == false)
+      {
+        if (m_frame_incoming == false)
+        {
+          if (byte != protocol_bytes::frame_boundary)
+          {
+            return;
+          }
+          else
+          {
+            /* start timeout. */
+            m_frame_incoming = true;
+          }
+        }
+        else
+        {
+          if (byte == protocol_bytes::frame_boundary)
+          {
+            m_frame_incoming = false;
+            m_frames_in++;
+          }
+        }
+
+        m_buffer.push_back(byte);
+      }
+    }
+
+    void recieve(const std::vector<uint8_t> buffer)
+    {
+
+      for (const auto c : buffer)
+      {
+        recieve(c);
+      }
+    }
 
   auto pop_frame(void)
   {
     std::vector<uint8_t> frame;
+    auto pop_frame(void)
+    {
+      std::vector<uint8_t> frame;
+
+      if (m_frames_in == 0)
+        return frame;
 
     if (m_frames_in == 0)
       return frame;
+      auto begin = m_buffer.begin();
+      auto end   = m_buffer.end();
+      auto sof   = std::find(begin, end, protocol_bytes::frame_boundary);
+      auto eof   = std::find(sof + 1, end, protocol_bytes::frame_boundary);
 
-    auto begin = m_buffer.begin();
-    auto end   = m_buffer.end();
-    auto sof   = std::find(begin, end, protocol_bytes::frame_boundary);
-    auto eof   = std::find(sof + 1, end, protocol_bytes::frame_boundary);
+      // Post increment eof since we need the end iterator to point one past the
+      // last element for std algorithms.
+      if (sof != end && eof++ != end)
+      {
+        frame.reserve(eof - sof);
+        std::copy(sof, eof, std::back_inserter(frame));
+        m_buffer.erase(sof, eof);
+        --m_frames_in;
+      }
 
-    // Post increment eof since we need the end iterator to point one past the
-    // last element for std algorithms.
-    if (sof != end && eof++ != end)
-    {
-      frame.reserve(eof - sof);
-      std::copy(sof, eof, std::back_inserter(frame));
-      m_buffer.erase(sof, eof);
-      --m_frames_in;
+      return frame;
     }
 
     return frame;
   }
 
+    if (m_frames_in > 0)
+    {
+      auto begin = m_buffer.begin();
+      auto end   = m_buffer.end();
+      auto sof   = std::find(begin, end, protocol_bytes::frame_boundary);
+      auto eof   = std::find(sof + 1, end, protocol_bytes::frame_boundary);
+
+      // Post increment eof since we need the end iterator to point one past the
+      // last element for std algorithms.
+      if (sof != end && eof++ != end)
+      {
+        frame.reserve(eof - sof);
+        std::copy(sof, eof, std::back_inserter(frame));
+        m_buffer.erase(sof, eof);
+        --m_frames_in;
+      }
+    }
+
+    return (frame);
+  }
+
+  auto busy(void) const noexcept { return m_frame_incoming; }
   void reset(void)
   {
     m_buffer.clear();
